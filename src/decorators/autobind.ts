0fe88387aa5a045e380ab8b1fdb0fa0954fa1bca
furxx2000 <furxx2000@gmail.com>
2022-09-28 22:53:39 +0800

    first commit

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,12 @@
+// Autobind decorator
+export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundfn = originalMethod.bind(this);
+      return boundfn;
+    },
+  };
+  return adjDescriptor;
+}
